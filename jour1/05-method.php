f820f465f62ca1386257b3174d44ba947812bef8
Malik <malik.h@webdevpro.net>
2022-05-23 12:08:31 +0200

    cas pratique

--- jour1/05-method.php
+++ jour1/05-method.php
@@ -30,3 +30,22 @@ class Rectangle{
 $r1 = new Rectangle(); // créer un objet
 $r1->calculAire(); // exécute la méthode calculAire() à partir de $r1
 // 305 !! 
+
+
+// créer un nouveau fichier 06-exo.php 
+// créer une class Livre
+// cette class contient 5 propriétés :
+// auteur Victor Hugo
+// nom Les Misérables
+// prix 20
+// devise euros
+// anneeParution 1862
+
+// cette class contient 2 méthodes :
+// méthode 1 : synopsis
+// var_dump() => "Victor Hugo a publié Les Misérables en 1862"
+
+// méthode 2 : vente
+// var_dump() "Pour acheter, Les Misérables il faudra débourser 20 euros"
+
+// exécuter les deux méthodes 
