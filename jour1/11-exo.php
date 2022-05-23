23f429d3cc80daebc48cb2080059bc0f71e06ba8
Malik <malik.h@webdevpro.net>
2022-05-23 16:48:04 +0200

    enoncé

--- jour1/11-exo.php
+++ jour1/11-exo.php
@@ -39,3 +39,28 @@ $guerrier->frapper($magicien);
 $guerrier->frapper($magicien);
 // affiche le contenu des deux objets
 var_dump($guerrier , $magicien);
+
+
+// https://www.w3resource.com/php-exercises/php-class-exercises.php
+// https://www.exoco-lmd.com/programmation-orientee-objet-(poo)/3-examens-corriges-programmation-orientee-objet-(poo)/
+// cas pratique : créer le fichier 12-exo.php
+
+// créer une class Maison
+// contient 2 propriétés privées
+// adresse = string = ""
+// isVendu = boolean = false 
+// setter et getter 
+
+// class Personne 
+// 1 propriété privée nom = string
+// setter getter
+// méthode acheter 
+// changer son status d'une maison  isVendu false => true 
+
+// la personne va pouvoir acheter la maison changer son status isVendu false => true 
+
+// créer une maison  adresse => "75 rue de Paris 75000 Paris"
+// créer une personne nom Alain
+// Alain va acheter cette maison 
+
+// vérifier que le status de la maison est désormais à la valeur true 
