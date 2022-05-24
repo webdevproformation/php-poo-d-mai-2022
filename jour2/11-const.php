1e6a43ac50f249fee7c109cc366f9ba143f9f019
Malik <malik.h@webdevpro.net>
2022-05-24 16:31:21 +0200

    énoncé

--- jour2/11-const.php
+++ jour2/11-const.php
@@ -1,5 +1,4 @@
 <?php // http://localhost/php-poo/jour2/11-const.php
-
 class Joueur{
     // possible de créer un type de propriété 
     // dont la valeur est initialisée une seule fois 
@@ -20,3 +19,21 @@ var_dump($j->surface());
 
 var_dump(Joueur::$hauteur); // utiliser une propriété statique publique hors de la class
 var_dump(Joueur::UNITE); // utiliser une constante de class publique hors de la class
+
+// cas pratique 
+
+// créer le fichier 12-exo.php 
+
+// class Role 
+// 3 constantes
+// ADMIN =>  2
+// REDACTEUR => 1
+// USER => 0
+
+// class Profil 
+// propriété private nom string
+// propriété rôle ne peuvent avoir QUE les rôles définis dans la class Rôle
+// créer les setter et getter nécessaires pour cette class
+
+
+
