0d5e5fb20770599fb2d65c7fcc3e441cb77ca4b1
Malik <malik.h@webdevpro.net>
2022-05-25 14:21:08 +0200

    page d'accueil

--- jour3-tp/index.php
+++ jour3-tp/index.php
@@ -1,9 +1,34 @@
 <?php // http://localhost/php-poo/jour3-tp
 declare(strict_types=1);
-
 spl_autoload_register(function($className){
     require "lib/" . $className . ".php";
 });
-
+if(!empty($_POST)){
+    $commentaire = new CRUD("commentaires");
+    $commentaire->create($_POST);
+    $_POST = [];
+}
+$formulaire = new Formulaire($_POST);
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Utilisation POO</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <h1>Ajouter un nouveau commentaire</h1>
+        <form method="POST">
+            <?php echo $formulaire->inputEmail("email" , "saisir votre email") ?>
+            <?php echo $formulaire->textarea("contenu" , "saisir votre commentaire") ?>
+            <?php echo $formulaire->inputSubmit() ?>
+        </form>
+    </div>
+</body>
+</html>
+
 
