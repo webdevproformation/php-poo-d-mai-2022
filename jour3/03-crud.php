dc6dd92035f8e5d80295e4f0130961719469087b
Malik <malik.h@webdevpro.net>
2022-05-25 12:33:23 +0200

    partie 1 crud

---
+++ jour3/03-crud.php
@@ -0,0 +1,57 @@
+<?php // http://localhost/php-poo/jour3/03-crud.php
+
+class CRUD{
+    private PDO $connexion ;
+    private string $table ;
+
+    public function __construct(string $tableName){
+        $dsn = "mysql:host=localhost;dbname=demo;charset=utf8";
+        $login = "root";
+        $password = ""; // "root" pour les utilisateurs de MacOS
+        //$options = [PDO::ATTR_DEFAULT_FETCH_MODE =>  PDO::FETCH_ASSOC];
+        $this->connexion = new PDO($dsn , $login, $password );
+        $this->connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);  
+        $this->table = $tableName;
+    }
+
+    public function create(array $data){
+        $keys = array_keys($data);
+        $keyDot = [] ;
+        foreach($keys as $k){
+            $keyDot[] = ":".$k ;
+        }
+        $sql = "INSERT INTO $this->table ( ". implode("," ,$keys) . " ) VALUES ( " . implode("," , $keyDot) .  " )";
+        $sth = $this->connexion->prepare($sql);
+        $sth->execute($data);
+        // var_dump($sql);
+        // INSERT INTO articles ( titre ) VALUES ("nouvel article") ;
+        // INSERT INTO $this->table ( titre ) VALUES ("nouvel article") ;
+        // $this->table => vient du __construct($tableName)
+        // titre => $data = ["titre"=> "nouvel article"]
+        // "nouvel article"
+    }
+
+    public function read(){
+        $sth = $this->connexion->prepare("SELECT * FROM $this->table");
+        $sth->execute();
+        return $sth->fetchAll();
+    }
+
+    public function update(){}
+
+    public function delete(){}
+}
+
+// l'avantage 
+
+/* $articles = new CRUD("articles");
+var_dump($articles->read()); // récupérer 
+$articles->create(["titre" => "encore un test" ]); // insérer
+$articles->delete(); // supprimer
+$articles->update(); // update */
+
+ $commentaires = new CRUD("commentaires");
+ $commentaires->create(["email" => "toto@yahoo.fr" , "contenu" => "je viens d'ajouter un commentaire via php"]); // insérer
+ var_dump($commentaires->read());
+$commentaires->delete(); // supprimer
+$commentaires->update(); // update 
